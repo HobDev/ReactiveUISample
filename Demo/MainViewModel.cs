87fd2e0e9e92727899db14962a08fe817c0ad0ed
paramjit <sparamjit92@gmail.com>
2019-08-05 13:13:35 +0530

    ObservableAsPropertyHelper

--- Demo/MainViewModel.cs
+++ Demo/MainViewModel.cs
@@ -19,13 +19,8 @@ namespace Demo
 
     public class MainViewModel : ReactiveObject
     {
-        IEnumerable<Company> companies;
-        public IEnumerable<Company> Companies
-        {
-            get => companies;
-            set => this.RaiseAndSetIfChanged(ref this.companies, value);
-        }
-
+        readonly ObservableAsPropertyHelper<IEnumerable<Company>> companies;
+        public IEnumerable<Company> Companies => this.companies.Value;
 
         string query;
         public string Query
@@ -38,7 +33,7 @@ namespace Demo
         public string NewCompany { get; set; }
 
         public ReactiveCommand<Unit, Unit> AddCompanyCommand { get; set; }
-        public ReactiveCommand<Unit, Unit> SearchCommand { get; set; }
+        public ReactiveCommand<Unit, IEnumerable<Company>> SearchCommand { get; set; }
 
         Realm _realm;
 
@@ -49,8 +44,13 @@ namespace Demo
             Companies = _realm.All<Company>();
 
             AddCompanyCommand = ReactiveCommand.CreateFromTask(async () => await AddButtonClicked());
-            SearchCommand = ReactiveCommand.CreateFromTask(async () => await SortCollection());
+            SearchCommand = ReactiveCommand.CreateFromObservable(
+                () =>
+                Observable
+                .StartAsync(SortCollection)
+                );
 
+            this.companies = SearchCommand.ToProperty(this, x => x.Companies);
 
 
             this.WhenAnyValue(x => x.Query).Where(query => !String.IsNullOrWhiteSpace(query)).Throttle(TimeSpan.FromSeconds(1)).Select(_ => Unit.Default).InvokeCommand(this, x => x.SearchCommand);
@@ -68,11 +68,11 @@ namespace Demo
             }
         }
 
-        async Task SortCollection()
+        async Task<IEnumerable<Company>> SortCollection()
         {
 
 
-            Companies = _realm.All<Company>().OrderBy(m => m.Name).Where(company => company.Name.ToLower().Contains(Query.ToLower()));
+            return _realm.All<Company>().OrderBy(m => m.Name).Where(company => company.Name.ToLower().Contains(Query.ToLower()));
 
         }
     }
