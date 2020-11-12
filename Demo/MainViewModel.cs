bb87e38f5d97d306357c9e77f9c480191f077687
Bernhard Millauer <bernhard.millauer@gmail.com>
2020-11-12 11:21:25 +0100

    Change Bindings from code behind to xaml. Add delete functionality. Change change-feed source

--- Demo/MainViewModel.cs
+++ Demo/MainViewModel.cs
@@ -1,69 +1,74 @@
 ﻿using System;
-using ReactiveUI;
 using System.Collections.ObjectModel;
-using System.Threading.Tasks;
 using System.Reactive;
-using DynamicData;
 using System.Reactive.Linq;
+using DynamicData;
+using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using Realms;
-using System.Linq;
 
 namespace Demo
 {
+  public class MainViewModel : ReactiveObject
+  {
+    private readonly SourceCache<Company, string> _Companies;
+    private readonly ReadOnlyObservableCollection<Company> _sortedCompanies;
 
-    public class MainViewModel : ReactiveObject
+    private readonly Realm _realm;
+
+    public MainViewModel()
     {
-        private readonly SourceCache<Company, string> _Companies;
-        private readonly ReadOnlyObservableCollection<Company> _sortedCompanies;
-        public ReadOnlyObservableCollection<Company> Companies => _sortedCompanies;
+      _realm = Realm.GetInstance();
+      var allCompanies = _realm.All<Company>();
 
+      allCompanies.SubscribeForNotifications((sender, changes, error) =>
+      {
+        _Companies.AddOrUpdate(allCompanies);
+      });
 
+      _Companies = new SourceCache<Company, string>(company => company.Id);
+      _Companies.AddOrUpdate(allCompanies);
 
-        [Reactive]
-        public string Query { get; set; }
+      var refreshObs = this.WhenAnyValue(x => x.Query).Throttle(TimeSpan.FromMilliseconds(500));
+      var dataConnection = _Companies.Connect();
 
-        [Reactive]
-        public string NewCompany { get; set; }
+      dataConnection
+       .AutoRefreshOnObservable(_ => refreshObs)
+       .ObserveOn(RxApp.MainThreadScheduler)
+       .Filter(m => Query == null || m.Name.IndexOf(Query, StringComparison.CurrentCultureIgnoreCase) >= 0)
+       .Bind(out _sortedCompanies)
+       .Subscribe();
 
-        public ReactiveCommand<Unit, Unit> AddCompanyCommand { get; set; }
-
-        Realm _realm;
-
-        public MainViewModel()
-        {
-
-            _realm = Realm.GetInstance();
-            _realm.RealmChanged += (s, e) => { _Companies.AddOrUpdate(_realm.All<Company>()); };
-            _Companies = new SourceCache<Company, string>(company => company.Id);
-            _Companies.AddOrUpdate(_realm.All<Company>());
-
-            var refreshObs = this.WhenAnyValue(x => x.Query).Throttle(TimeSpan.FromMilliseconds(500));
-            var dataConnection = _Companies.Connect();
-
-            dataConnection
-                .AutoRefreshOnObservable(_ => refreshObs)
-                .ObserveOn(RxApp.MainThreadScheduler)
-                .Filter(m => Query == null || m.Name.IndexOf(Query, StringComparison.CurrentCultureIgnoreCase) >= 0)
-                .Bind(out _sortedCompanies)
-                .Subscribe();
-
-            AddCompanyCommand = ReactiveCommand.CreateFromTask(async () => await AddCompany());
-
-        }
-
-        async Task AddCompany()
-        {
-            if (!string.IsNullOrWhiteSpace(NewCompany))
-            {
-                _realm.Write(() =>
-                {
-                    _realm.Add(new Company { Name = NewCompany });
-                });
-                NewCompany = string.Empty;
-            }
-
-        }
+      AddCompanyCommand = ReactiveCommand.Create(AddCompany);
+      DeleteCompanyCommand = ReactiveCommand.Create<Company>(DeleteCompany);
     }
-}
 
+    public ReadOnlyObservableCollection<Company> Companies => _sortedCompanies;
+
+    [Reactive]
+    public string Query { get; set; }
+
+    [Reactive]
+    public string NewCompany { get; set; }
+
+    public ReactiveCommand<Unit, Unit> AddCompanyCommand { get; set; }
+
+    public ReactiveCommand<Company, Unit> DeleteCompanyCommand { get; set; }
+
+    private void AddCompany()
+    {
+      if (!string.IsNullOrWhiteSpace(NewCompany))
+      {
+        _realm.Write(() => { _realm.Add(new Company { Name = NewCompany }); });
+        NewCompany = string.Empty;
+      }
+    }
+
+    private void DeleteCompany(Company company)
+    {
+      // remove the item from the collection because the update loop only adds/updates items
+      _Companies.Remove(company);
+      _realm.Write(() => { _realm.Remove(company); });
+    }
+  }
+}
